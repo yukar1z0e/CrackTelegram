be3a2438498bf324f567fb126ab95b1720be2f8c
yukar1z0e <yakumozoe@outlook.com>
2019-08-09 12:53:38 +0800

    "Telegram Program Start"
    Signed-off-by: yukar1z0e <yakumozoe@outlook.com>

---
+++ crackTelegram/app/src/main/java/com/yukar1z0e/cracktelegram/crackmain.java
@@ -0,0 +1,50 @@
+package com.yukar1z0e.cracktelegram;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.FileObserver;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.MotionEvent;
+
+import java.text.Format;
+import java.util.Collections;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.TimeUnit;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.findField;
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class crackmain implements IXposedHookLoadPackage {
+    private XC_LoadPackage.LoadPackageParam lpparam = null;
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (loadPackageParam.packageName.contains("org.telegram")) {
+            findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    lpparam = loadPackageParam;
+                    test();
+                }
+            });
+        }
+    }
+
+    public void test() {
+        Log.d("crackTelegram", "package name: " + lpparam.packageName);
+    }
+}
