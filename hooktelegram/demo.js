c7cb2e9e0afcd78358f8bb00db895f3fd621a0a4
yukar1z0e <yakumozoe@outlook.com>
2019-08-14 13:28:19 +0800

    hook native

--- hooktelegram/demo.js
+++ hooktelegram/demo.js
@@ -1,12 +1,10 @@
-// Hook Demo
-Intercepter.attach(Module.findExportByName("libc.so","open"),{
-   onEnter:function (args) {
-       log("open() called!")
-   },
-    onLeave:function (retval) {
-        
+Interceptor.attach(Module.findExportByName("libc.so" , "open"), {
+    onEnter: function(args) {
+        log("open() called!")
+    },
+    onLeave:function(retval){
+    
     }
 });
 
-
-// frida -R -n org.potato.messenger -l demo.js -o android.util.log.log
+// frida -U -n org.potato.messenger -l demo.js -o android.util.log.log
